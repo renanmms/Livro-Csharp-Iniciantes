916dae07bc793b9f4bb166192fb626462384a39f
Renan Martins <renanmartins1999@hotmail.com>
2022-04-12 23:19:17 -0300

    Adicionando capitulo 3

---
+++ Cap2/Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using SistemaBancario;
+
+var objetoGerente = new Gerente(); // Instanciamento de classe
+
+// sbyte a = 2;
+// byte b = 3;
+// short c = 4;
+Console.WriteLine("Tamanho das variáveis");
+Console.WriteLine("sbyte.MaxValue = {0}", sbyte.MaxValue);
+Console.WriteLine($"sbyte.MinValue = {sbyte.MinValue}");
+
+Console.WriteLine($"byte.MaxValue = {byte.MaxValue}");
+Console.WriteLine($"byte.MinValue = {byte.MinValue}");
+
+Console.WriteLine($"short.MaxValue = {short.MaxValue}");
+Console.WriteLine($"short.MinValue = {short.MinValue}");
+
+Console.WriteLine();
+
+// tipo_de_dados nome_da_variavel = valor;
+
+
+
+Console.ReadKey();
+
+
+namespace SistemaBancario {
+    public class Gerente { 
+        public string Nome {get; set; }
+        public int Idade { get; set; }
+        public int Cpf { get; set; }
+        public void AprovarHorasDeFuncionarios(int cpf){
+
+        }
+    }
+}
--- Classes/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-namespace SistemaBancario {
-    public class Gerente { 
-        public string Nome {get; set; }
-        public int Idade { get; set; }
-        public int Cpf { get; set; }
-    }
-}
