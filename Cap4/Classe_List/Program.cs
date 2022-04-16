aaa5d07d032a4c204b9c030df3b40764ee5a4bbe
Renan Martins <renanmartins1999@hotmail.com>
2022-04-16 13:45:23 -0300

    Adicionando classe list

---
+++ Cap4/Classe_List/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> nomesFuncionarios = new List<string>();
+
+nomesFuncionarios.Add("Maria");
+nomesFuncionarios.Add("João");
+nomesFuncionarios.Add("André");
+nomesFuncionarios.Add("Flávia");
+
+nomesFuncionarios.Remove("Flávia");
+nomesFuncionarios.RemoveAt(0);
+
+Console.WriteLine();
+foreach(string pessoa in nomesFuncionarios){
+    Console.WriteLine($"{pessoa}");
+}
