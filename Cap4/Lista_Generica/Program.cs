903b440614dd31554fbb91ad72632dd0d20b7071
Renan Martins <renanmartins1999@hotmail.com>
2022-04-16 13:46:39 -0300

    Adicionando lista generica

---
+++ Cap4/Lista_Generica/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Declarando lista do tipo inteiro
+ListaGenerica<int> lista1 = new ListaGenerica<int>();
+
+// Declarando lista do tipo string
+ListaGenerica<string> lista2 = new ListaGenerica<string>();
+
+// Declarando lista de uma classe
+ListaGenerica<ExampleClass> lista3 = new ListaGenerica<ExampleClass>();
+
+private class ExampleClass {}
+
+public class ListaGenerica<T>{
+    public void Adicionar(T input){ }
+}
