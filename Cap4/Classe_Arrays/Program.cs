9d1d9ddcc4af7fc373796b67450ae4c54c882f34
Renan Martins <renanmartins1999@hotmail.com>
2022-04-16 13:43:38 -0300

    Adicionando cap.4 e classe arrays

---
+++ Cap4/Classe_Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+int[] primeirArray = new int[5];
+int[] segundArray = new int[] {1, 3, 5, 7, 9};
+
+
+string s;
+
+for(int i = 0; i < primeirArray.Length - 1; i++){
+    primeirArray[i] = Convert.ToInt32(ReadLine());
+}
+
+// somando elementos
+int soma = 0;
+foreach(int n in primeirArray){
+    soma += n;
+}
+
+WriteLine($"Soma dos elementos do primeiro Array: {soma}");
+WriteLine($"Acessando a posição 0 -> {segundArray[0]}");
+
+
