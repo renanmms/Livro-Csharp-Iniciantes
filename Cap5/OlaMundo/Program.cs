f155887bc0c86acdf68840d419ed21ecd5ac2fb0
Renan Martins <renanmartins1999@hotmail.com>
2022-04-16 13:49:08 -0300

    Adicionando cap.5

---
+++ Cap5/OlaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Criamos o objeto Pedido herdado da classe InteracaoUsuario
+InteracaoUsuario Pedido = new InteracaoUsuario();
+
+// Invocamos os métodos do objeto Pedido herdado da classe Mãe InteracaoUsuario
+var Digitado = Pedido.SolicitarDigitacao();
+Pedido.MostrarDigitacao(Digitado);
+
+
+class InteracaoUsuario {
+    public string SolicitarDigitacao()
+    {
+        return Console.ReadLine();
+    }
+    public void MostrarDigitacao(string Digitado)
+    {
+        Console.WriteLine(Digitado);
+    }
+}
