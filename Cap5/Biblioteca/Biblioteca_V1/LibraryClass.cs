5a79bb4119779509facf0ccc9c73a24f95d01636
Renan Martins <renanmartins1999@hotmail.com>
2022-04-17 22:52:18 -0300

    Adicionando exemplo de Biblioteca

---
+++ Cap5/Biblioteca/Biblioteca_V1/LibraryClass.cs
@@ -0,0 +1,37 @@
+namespace LibraryClass {
+    public class Biblioteca {
+        private string _Titulo;
+        private string _Autor;
+        private int _Paginas;
+        private bool _Status;
+
+        // Construtor sem parâmetros
+        public Biblioteca(){
+
+        }
+
+        public Biblioteca(string Titulo, string Autor, int Paginas, bool Status){
+            _Titulo = Titulo;
+            _Autor = Autor;
+            _Paginas = Paginas;
+            _Status = Status;
+        }
+
+        public string Titulo {
+            get {return _Titulo;} set { _Titulo = value;}
+        }
+
+        public string Autor {
+            get {return _Autor;} set {_Autor = value;}
+        }
+        public int Paginas 
+        {
+            get {return _Paginas;} set {_Paginas = value;}
+        }
+
+        public bool Status 
+        {
+            get{ return _Status;} set {_Status = value;}
+        }
+    }
+}
---
+++ Cap5/Biblioteca/Biblioteca_V1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LibraryClass;
+
+Biblioteca MinhaBiblioteca = new Biblioteca();
+MinhaBiblioteca.Autor = "Des Dearlove";
+MinhaBiblioteca.Titulo = "O Estilo Bill Gates de Gerir";
+MinhaBiblioteca.Paginas = 203;
+MinhaBiblioteca.Status = true;
+
+Console.WriteLine($"Autor: {MinhaBiblioteca.Autor}");
+Console.WriteLine($"Titulo: {MinhaBiblioteca.Titulo}");
+Console.WriteLine($"Paginas: {MinhaBiblioteca.Paginas}");
+Console.WriteLine($"Status: {MinhaBiblioteca.Status}");
+Console.WriteLine();
+
