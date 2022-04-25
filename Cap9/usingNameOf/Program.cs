66824dac61693de2ed6819f358ce65f5da1c1be7
Renan Martins <renanmartins1999@hotmail.com>
2022-04-24 22:51:02 -0300

    Adicionando capitulo 9

---
+++ Cap9/usingNameOf/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+try
+{
+    string produto = "peixe";
+    DateTime lancto = new DateTime(2010, 5, 15);
+    WriteLine($"o {produto} tem {idade(lancto.Year)} anos");
+    WriteLine($"o {produto} tem {idade(2023)} anos");
+}
+catch (Exception ex)
+{
+    WriteLine(ex.Message);
+}
+
+static int idade(int pAno)
+{
+    if(pAno > DateTime.Today.Year)
+    {
+        throw new Exception($"ano invalido {nameof(pAno)}");
+    }
+    return DateTime.Today.Year - pAno;
+}
+
+static string PescaAutorizada(int pMes, int pDia){
+    if(pDia < 16){
+        throw new Exception($"primeira quinzena {nameof(pDia)}");
+    }
+    if(pMes >= 5 && pMes <= 8){
+        throw new Exception($"pesca no inverno {nameof(pMes)}");
+    }
+}
---
+++ Cap9/usingStatic/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using static System.Math;
+using static System.String;
+
+// System.Math
+// raiz quadrada
+WriteLine($"Raiz quadrada: {Sqrt(3*3 + 4*4)}");
+
+// Retorna o valor absoluto
+WriteLine($"Valor absoluto positivo: {Abs(1234.78)}");
+WriteLine($"Valor absoluto negativo: {Abs(-1234.78)}");
+WriteLine($"Valor Máximo: {Max(100, 590)}");
+WriteLine($"Valor Mínimo: {Min(100, 590)}");
+
+// Arredonda um valor para o inteiro mais próximo ou para o número especificado de casas decimais
+WriteLine($"Arrendonda com 1 casa decimal: {Round(Math.PI, 1)}"); // 3,1
+WriteLine($"Arrendonda com 2 casas decimais: {Round(Math.PI, 2)}"); // 3,14
+
+// System.String
+WriteLine($"Tamanho da String: {"Livro de C#".Length}");
+
+string livro = "Livro de EF Core";
+
+WriteLine($"Concatena 2 strings: {Concat("Livro de C#,", "Livro de EF Core")}");
+WriteLine($"{livro.Insert(6, "avançado ").ToUpper()}");
