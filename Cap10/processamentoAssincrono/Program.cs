35d403864c3e868fa7517435536e65fb2986e96e
Renan Martins <renanmartins1999@hotmail.com>
2022-04-27 22:43:20 -0300

    Adicionando processamento assíncrono

---
+++ Cap10/processamentoAssincrono/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+AccessTheWebAsync();
+
+async Task<int> AccessTheWebAsync() {
+	HttpClient client = new HttpClient(); 
+
+	Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+	
+	DoIndependentWork();
+
+	string urlContents = await getStringTask;
+
+	return urlContents.Length;
+
+}
+
+void DoIndependentWork()
+{
+	Console.WriteLine("Working . . . . . . \r\n");
+}
