9fdd97f5de3cf2c52bfab8ef0ccef9ee1f413399
Renan Martins <renanmartins1999@hotmail.com>
2022-04-29 13:02:43 -0300

    Adicionando exemplos de prog. assincrona

---
+++ Cap10/processamentoAssincrono2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+meuNome();
+Console.WriteLine("função 1 foi executada");
+
+meuSobrenome();
+Console.WriteLine("função 2 foi executada");
+
+
+Console.WriteLine("");
+
+async Task meuNome(){
+    Task.Delay(5000).Wait();
+}
+
+async Task meuSobrenome(){
+    Task.Delay(1000).Wait();
+}
