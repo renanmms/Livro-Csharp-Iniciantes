2d15ff876a9c1c7d4f75d893b2fcd21a4830faf8
Renan Martins <renanmartins1999@hotmail.com>
2023-06-03 21:14:09 -0300

    Adicionando exemplo de Pilha

---
+++ Cap4/Classe_Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿var pratosSujos = new Stack<string>();
+
+pratosSujos.Push("Prato 1");
+pratosSujos.Push("Prato 2");
+pratosSujos.Push("Prato 3");
+
+Console.WriteLine($"Pratos Sujos: {pratosSujos.Peek()}"); // Pratos Sujos: Prato 3
+
+var pratosLimpos = new Stack<string>();
+
+
+Console.WriteLine("Lavando prato...");
+pratosLimpos.Push(LavarPrato(pratosSujos));
+
+Console.WriteLine($"Pratos Limpos: {pratosLimpos.Peek()}"); // Pratos Limpos: Prato 3
+Console.WriteLine($"Pratos Sujos: {pratosSujos.Peek()}"); // Pratos Sujos: Prato 2
+
+string LavarPrato(Stack<string> prato){
+    return prato.Pop();
+}
